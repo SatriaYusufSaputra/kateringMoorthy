f8143d550ab32d7dcefa7a7fe67e8e1e805e7e99
Satria Yusuf Saputra <satriasys2@gmail.com>
2026-01-21 10:44:23 +0700

    Fix variable name in menu query and improve HTML structure for menu display

--- pages/menu.php
+++ pages/menu.php
@@ -2,7 +2,7 @@
 session_start();
 include '../koneksi.php';
 
-$menu = mysqli_query($conn, "SELECT * FROM menu ORDER BY id DESC");
+$menu = mysqli_query($koneksi, "SELECT * FROM menu ORDER BY id DESC");
 ?>
 
 <!DOCTYPE html>
@@ -14,6 +14,28 @@ $menu = mysqli_query($conn, "SELECT * FROM menu ORDER BY id DESC");
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body>
-    
+    <div>
+        <h1>
+            Menu Katering
+        </h1>
+        <div>
+            <?php while($row = mysqli_fetch_assoc($menu)) : ?>
+                <div>
+                    <img src="../assets/img/<?= $row['gambar']; ?>" >           
+                    <div>
+                        <h2><?= $row['nama_menu']; ?></h2>
+                        <p><?= $row['deskripsi']; ?></p>
+                        <p>Rp <?= number_format($row['harga']) ?></p>
+
+                        <?php if(isset($_SESSION['user'])) : ?>
+                            <a href="../proses/tambah_keranjang.php?id=<?= $row['id']; ?>"> + Tambah ke Keranjang</a>
+                        <?php else : ?>
+                            <a href="../login.php"> Login Untuk Pesan</a>
+                            <?php endif; ?>
+                    </div>
+                </div>
+            <?php endwhile; ?>
+        </div>
+    </div>
 </body>
 </html>
